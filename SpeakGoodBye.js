eb526d42b6810f616b36932fbdb2aced82006e49
SrushtiPatkar <114673565+SrushtiPatkar@users.noreply.github.com>
2023-08-07 11:02:28 +0530

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,5 +1,9 @@
-
-var speakWord = "Good Bye";
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
+(function (window) {
+  var speakWord = "Good Bye";
+  var byeSpeaker = {
+    speak: function (name) {
+      console.log(speakWord + " " + name);
+    }
+  };
+  window.byeSpeaker = byeSpeaker;
+})(window);
