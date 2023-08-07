e52ec241bea4680d3615a864b81d9ead2f8f77f1
SrushtiPatkar <114673565+SrushtiPatkar@users.noreply.github.com>
2023-08-07 10:46:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-(function (window) {
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function () {
 
-  for (var i in names) {
-    var firstLetter = names[i].charAt(0).toLowerCase();
-    if (firstLetter === 'j') {
-      byeSpeaker.speak(names[i]);
-    } else {
-      helloSpeaker.speak(names[i]);
-    }
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+// STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
+// Loop over the names array and say either 'Hello' or "Good Bye"
+// using either the helloSpeaker's or byeSpeaker's 'speak' method.
+// See Lecture 50, part 1
+for (var i = 0; i < names.length; i++) {
+
+  var firstLetter = names[i].charAt(0).toLowerCase();
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
   }
-})(window);
+}
+
+})();
